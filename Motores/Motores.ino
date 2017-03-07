d03cbfc147cf7d2ddf61fa8c69f79410d4e51ccc
CristianGaitanG <cristiangaitan09@gmail.com>
2017-03-07 14:30:10 -0500

    Add files via upload

---
+++ Motores/Motores.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+#define SERVO1_PWM 10
+#define SERVO2_PWM 9
+
+Servo servo_1;
+Servo servo_2;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Simple Motor Shield sketch");
+  servo_1.attach(SERVO1_PWM);
+  servo_2.attach(SERVO2_PWM);
+}
+
+void loop() {
+  servo_1.write(130);  
+  delay(1000);
+  servo_2.write(0);
+  delay(1000);
+
+  servo_2.write(65);
+  delay(1000);
+  servo_1.write(50);
+  delay(1000);
+}
